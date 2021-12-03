66d4271fbabf021792e736a50a1d1ca1b328cba1
Anton <56966986+benbi73tt@users.noreply.github.com>
2021-12-03 16:32:19 +0400

    bubbleSort

--- src/main/java/ru/madbrains/simpleList/ListOperation.java
+++ src/main/java/ru/madbrains/simpleList/ListOperation.java
@@ -2,11 +2,7 @@ package ru.madbrains.simpleList;
 
 import java.util.*;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.*;
-import java.util.stream.IntStream;
 
 public class ListOperation<T> implements SimpleList<T> {
 
